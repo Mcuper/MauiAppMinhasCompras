3ef6085be2dd719ef29dd4f1efd10987f3957b51
rapha <rapha@RAPHAEL>
2026-02-17 15:26:07 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace MauiAppMinhasCompras.Models
+{
+    internal class Produto
+    {
+    }
+}
